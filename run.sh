7cfaefb2a3f758b4e81b32b55fe5f5b6a4c96217
Maksym Kononenko <maksym.kononenko@synapse.com>
2018-06-27 13:09:30 +0300

    deploy fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker stop hello_world_war
-docker run -it -d -p 8080:8080 --rm --name hello_world_war nexus.phyzeek.com/jenkins-swarm:latest
+docker run -it -d -p 8080:8080 --rm --name hello_world_war catalinalab/jenkins-swarm:latest
